e082ea10f785f4cdbef83cb5af4ddf254e4b9fb1
Carlos Bentin <cbentin12@gmail.com>
2021-12-08 05:54:48 -0500

    added file with memory management methods 1) implemented initialization function for main data structure

---
+++ mem_mgmt.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * init_cmd - initialize main struct
+ * @_cmd: main struct
+ * @envir: envirorment list
+ * Return: none - void
+ */
+void init_cmd(struct command *_cmd, char **envir)
+{
+	_cmd->env_list = envir;
+	_cmd->input = NULL;
+	_cmd->path = parse_str(_getenv("PATH"), ":\n");
+	_cmd->full_cmd_path = NULL;
+	_cmd->status_code = BASE_STATUS;
+	_cmd->input_type = F_NOT_TERMINAL;
+	_cmd->lines_counter = 0;
+	_cmd->flags = 0;
+	_cmd->exit_code = 0;
+	_cmd->size = 0;
+}
