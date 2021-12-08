0c85cebd3074a4bf73bf49d8c362d1811ce04308
Humberto Tejada <humtej1204@gmail.com>
2021-12-08 05:48:49 -0500

    Adding our own executable functions for our own shell

---
+++ built_ins.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * exit_shell - exit shell, sets flag value to -1
+ * @_cmd: main data struct
+ * Return: none - void function
+ */
+void exit_shell(struct command *_cmd)
+{
+	_cmd->status_code = EXIT_STATUS;
+	if (_cmd->cmd[1] != NULL)
+		_cmd->exit_code = atoi(_cmd->cmd[1]);
+}
+
+/**
+ * ppath - print parsed path to console
+ * @_cmd: main data struct
+ * Return: none - void function
+ */
+void ppath(struct command *_cmd)
+{
+	int i = 0;
+
+	while (_cmd->path[i])
+	{
+		printf("PATH [%d] -> %s\n", i, _cmd->path[i]);
+		i++;
+	}
+}
+
+/**
+ * clear_screen - clears screens
+ * @_cmd: main data struct
+ * Return: none - void function
+ */
+void clear_screen(struct command *_cmd)
+{
+	(void)_cmd;
+	clear(void);
+}
+
+/**
+ * change_dir - changes directory to [path]
+ * @_cmd: main data struct
+ * Return: none - void function
+ */
+void change_dir(struct command *_cmd)
+{
+	char *new_dir, *old_dir, *curr_dir;
+	struct stat _stat;
+	int free_mem = 0;
+
+	old_dir = _getenv("OLDPWD");
+	curr_dir = _getenv("PWD");
+
+	if (_cmd->cmd[1] == NULL)
+	{
+		new_dir = _getenv("HOME");
+	}
+	else if (strncmp(_cmd->cmd[1], "-", 1) == 0)
+	{
+		new_dir = old_dir;
+		printf("%s\n", new_dir);
+	}
+	else
+	{
+		new_dir = get_dir_pathname(_cmd->cmd[1], curr_dir);
+		free_mem = 1;
+	}
+
+	if (stat(new_dir, &_stat) == 0 && S_ISDIR(_stat.st_mode))
+	{
+		_setenv("OLDPWD", curr_dir, 1);
+		chdir(new_dir);
+		_setenv("PWD", new_dir, 1);
+	}
+	else
+		_cmd->status_code = DIR_NOTFOUND;
+
+	if (free_mem == 1)
+		free(new_dir);
+}
