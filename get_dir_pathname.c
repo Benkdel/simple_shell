8b086206243f6b2b6e26b336f29ca004e6776e03
Humberto Tejada <humtej1204@gmail.com>
2021-12-08 05:56:21 -0500

    Adding function to get the current directory

---
+++ get_dir_pathname.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_dir_pathname - build full path for directory
+ * @target_dir: argument of cd - directory to go to
+ * @current_dir: pwd
+ * Return: pointer to a string with full directory pathname
+ */
+char *get_dir_pathname(char *target_dir, char *current_dir)
+{
+	ssize_t wld_c;
+	char *dir_pathname;
+
+	/* check if "../" are found */
+	wld_c = find_subnstr(target_dir, "../", 3);
+	if (wld_c != -1)
+	{
+		/* "../" found - go back dirs as ../ and rebuild path*/
+		/* TODO */
+	}
+	else
+	{
+		if (find_subnstr(target_dir, "/", 1) == 0)
+			dir_pathname = _concat(2, target_dir, "/");
+		else
+			dir_pathname = _concat(3, current_dir, "/", target_dir);
+	}
+	return (dir_pathname);
+}
