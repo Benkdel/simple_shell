3b7fd021075c38c2aa15287c968890aac5de5e66
Humberto Tejada <humtej1204@gmail.com>
2021-12-09 10:06:29 -0500

    Correcting gz_filename

--- create_man.sh
+++ create_man.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp man_1_simple_shell man_1_simple_shell.1
 gzip -q man_1_simple_shell.1
-sudo mv man_1_simple_shell.gz /usr/share/man/man1/
+sudo mv man_1_simple_shell.1.gz /usr/share/man/man1/
 man man_1_simple_shell 1
